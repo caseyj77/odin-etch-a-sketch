238a111b3291d0f41754448052ecf08150d78943
Zorishimo <caseyj77@gmail.com>
2024-04-22 13:55:45 -0500

    refactoring code to make it easier to read

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 const GRID = 600; 
-let rows = 16;
-let cols = 16;
+let squaresPerSide = 16
 
 //create the grids within the sketchArea this is dynamic based on the size of the GRID
 const sketchArea = document.querySelector("#screen-area"); 
-sketchArea.style.width = `${GRID}px`;
-sketchArea.style.height = `${GRID}px`;
+sketchArea.style.width = sketchArea.style.height = `${GRID}px`;
 
 // for loop that creates the cells using the rows and cols variables to create cells inside of the 'screen-area' div
+
+
+
 function createGridCells() {
-    for (let i = 0; i < (rows * cols); i++) {
+    const numSquares = (squaresPerSide * squaresPerSide);
+    const widthOrHeight = `${(GRID / squaresPerSide)-2}px`;
+
+    for (let i = 0; i < numSquares; i++) {
         const gridCell = document.createElement("div")
 
-        gridCell.style.width = `${(GRID / cols)-2}px`;
-        gridCell.style.width = `${(GRID / rows)-2}px`; //had to oadd the -2 to account for the border size//
+        gridCell.style.width = gridCell.style.width = widthOrHeight;
         gridCell.classList.add('cell');
-
+        
         sketchArea.appendChild(gridCell);
 
         gridCell.addEventListener('mouseover', changeColor); // event listenr looking for the mouse to enter the cell and then changing its color
@@ -25,5 +28,8 @@ function createGridCells() {
 function changeColor() {
     this.style.backgroundColor = "black";
 }
+
+
+
 
 createGridCells();
