33758d1ec7950ec1c44bf1396db6d5ee7a3f1afa
Zorishimo <caseyj77@gmail.com>
2024-04-22 15:30:36 -0500

    complted project

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 const GRID = 600; 
 let squaresPerSide = 16
+let randomColorMode = false; //tracks whether the random button has been clicked
 
 //create the grids within the sketchArea this is dynamic based on the size of the GRID
 const sketchArea = document.querySelector("#screen-area"); 
 sketchArea.style.width = sketchArea.style.height = `${GRID}px`;
-
-// for loop that creates the cells using the rows and cols variables to create cells inside of the 'screen-area' div
-
-
 
 function createGridCells() {
     const numSquares = (squaresPerSide * squaresPerSide);
@@ -21,15 +18,46 @@ function createGridCells() {
         
         sketchArea.appendChild(gridCell);
 
-        gridCell.addEventListener('mouseover', changeColor); // event listenr looking for the mouse to enter the cell and then changing its color
+        gridCell.addEventListener('mouseover', changeColor); // event listener looking for the mouse to enter the cell and then changing its color
     }
 }
 
 function changeColor() {
-    this.style.backgroundColor = "black";
+    if (randomColorMode) {
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        this.style.backgroundColor = randomColor;
+    } else {
+        this.style.backgroundColor = "black";
+    }
+    
 }
 
+function updateGrid() {
+    const input = document.getElementById('grid-size');
+    const newSize = parseInt(input.value, 10);
 
+    if (!isNaN(newSize) && newSize >0) {
+        squaresPerSide = newSize;
+        clearGrid();
+        createGridCells();
+    } else {
+        alert("Please enter a number greter than 0")
+    }
+}
 
+function clearGrid() {
+    sketchArea.innerHTML = "";
+}
+
+function resetGrid() {
+    squaresPerSide = 16;
+    clearGrid();
+    createGridCells();
+}
+
+document.getElementById('reset').addEventListener('click', resetGrid);
+document.getElementById('random').addEventListener('click', function(){
+    randomColorMode = !randomColorMode;
+})
 
 createGridCells();
