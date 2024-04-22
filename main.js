323810e964fb2002f537dd2f4b839dd5f77ac2cd
Zorishimo <caseyj77@gmail.com>
2024-04-22 15:38:02 -0500

    added comments

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function createGridCells() {
 
 function changeColor() {
     if (randomColorMode) {
-        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`; //used google (css-tricks.com) to help with this feature had no idea how to create a random color
         this.style.backgroundColor = randomColor;
     } else {
         this.style.backgroundColor = "black";
@@ -32,32 +32,43 @@ function changeColor() {
     
 }
 
+// This function updates the size of the grid based on user input
 function updateGrid() {
+    // Get the input element where the user enters the new grid size
     const input = document.getElementById('grid-size');
+    
+    // Parse the input value to an integer with base 10
     const newSize = parseInt(input.value, 10);
 
-    if (!isNaN(newSize) && newSize >0) {
+    // Check if the parsed value is not a number or is less than or equal to 0
+    if (!isNaN(newSize) && newSize > 0) {
+        // If the input is valid, update the number of squares per side
         squaresPerSide = newSize;
+        
+        // Clear the current grid
         clearGrid();
+        
+        // Create a new grid with the updated size
         createGridCells();
     } else {
-        alert("Please enter a number greter than 0")
+        // If the input is not a valid number or is less than or equal to 0, show an alert
+        alert("Please enter a number greater than 0");
     }
 }
 
 function clearGrid() {
-    sketchArea.innerHTML = "";
+    sketchArea.innerHTML = ""; // removes grid
 }
 
 function resetGrid() {
     squaresPerSide = 16;
     clearGrid();
-    createGridCells();
+    createGridCells(); // resets grid to 16 by 16
 }
 
 document.getElementById('reset').addEventListener('click', resetGrid);
 document.getElementById('random').addEventListener('click', function(){
-    randomColorMode = !randomColorMode;
+    randomColorMode = !randomColorMode; // this toggles randomeColorMode on and off
 })
 
 createGridCells();
